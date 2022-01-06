3e0cdd7f5e65f8a262a5c7449cc4f18599d1a7e7
Alex Tam <alextam19@protonmail.com>
2022-01-06 11:34:20 +1000

    Update slice plotting

--- draw.jl
+++ draw.jl
@@ -10,56 +10,44 @@ using DelimitedFiles
 function draw_heat(x, y, U, V, ϕ, i, Lx, Ly)
     gr(); plot() # Load GR plotting backend and clear previous plots
     default(titlefont = (18, "Computer Modern"), guidefont = (26, "Computer Modern"), tickfont = (18, "Computer Modern"))
-    heatmap(x,y,transpose(U), xlabel = L"$x$", ylabel = L"$y$", margin=2mm, aspect_ratio=:equal, xlims=(0,Lx), ylims =(0,Ly), tick_direction=:out, c=:plasma, clims=(0.0, 1.0))
+    heatmap(x,y,transpose(U), xlabel = L"$x$", ylabel = L"$y$", margin=3mm, aspect_ratio=:equal, xlims=(0,Lx), ylims =(0,Ly), tick_direction=:out, c=:plasma, clims=(0.0, 1.0))
     savefig("u-$i.pdf")
-    heatmap(x,y,transpose(V), xlabel = L"$x$", ylabel = L"$y$", margin=2mm, aspect_ratio=:equal, xlims=(0,Lx), ylims =(0,Ly), tick_direction=:out, c=:plasma)
+    heatmap(x,y,transpose(V), xlabel = L"$x$", ylabel = L"$y$", margin=3mm, aspect_ratio=:equal, xlims=(0,Lx), ylims =(0,Ly), tick_direction=:out, c=:plasma)
     savefig("V-$i.pdf")
-    heatmap(x,y,transpose(ϕ), xlabel = L"$x$", ylabel = L"$y$", margin=2mm, aspect_ratio=:equal, xlims=(0,Lx), ylims =(0,Ly), tick_direction=:out, c=:plasma)
+    heatmap(x,y,transpose(ϕ), xlabel = L"$x$", ylabel = L"$y$", margin=3mm, aspect_ratio=:equal, xlims=(0,Lx), ylims =(0,Ly), tick_direction=:out, c=:plasma)
     savefig("phi-$i.pdf")
 end
 
 function draw_heat(x, y, U, ϕ, i, Lx, Ly)
     gr(); plot() # Load GR plotting backend and clear previous plots
     default(titlefont = (18, "Computer Modern"), guidefont = (26, "Computer Modern"), tickfont = (18, "Computer Modern"))
-    heatmap(x,y,transpose(U), xlabel = L"$x$", ylabel = L"$y$", margin=2mm, aspect_ratio=:equal, xlims=(0,Lx), ylims =(0,Ly), tick_direction=:out, c=:plasma, clims=(0.0, 1.0))
+    heatmap(x,y,transpose(U), xlabel = L"$x$", ylabel = L"$y$", margin=3mm, aspect_ratio=:equal, xlims=(0,Lx), ylims =(0,Ly), tick_direction=:out, c=:plasma, clims=(0.0, 1.0))
     savefig("u-$i.pdf")
-    heatmap(x,y,transpose(ϕ), xlabel = L"$x$", ylabel = L"$y$", margin=2mm, aspect_ratio=:equal, xlims=(0,Lx), ylims =(0,Ly), tick_direction=:out, c=:plasma)
+    heatmap(x,y,transpose(ϕ), xlabel = L"$x$", ylabel = L"$y$", margin=3mm, aspect_ratio=:equal, xlims=(0,Lx), ylims =(0,Ly), tick_direction=:out, c=:plasma)
     savefig("phi-$i.pdf")
 end
 
-"Plot solutions as slices"
-function draw_slices(x, y, U, V, ϕ, i, nx, ny, Lx, Ly)
+"Density slice plots"
+function draw_slices(x, y, nx, ny, Lx, Ly, plot_times)
     gr(); plot() # Load GR plotting backend and clear previous plots
-    default(titlefont = (18, "Computer Modern"), guidefont = (26, "Computer Modern"), tickfont = (18, "Computer Modern"))
-    plot(x, U[:,ny], xlabel = L"$x$", ylabel = L"$u(x,10,t)$", margin=2mm, legend = false, xlims=(0,Lx))
-    savefig("u_slice_x-$i.pdf")
-    plot(x, V[:,ny], xlabel = L"$x$", ylabel = L"$V(x,10,t)$", margin=2mm, legend = false, xlims=(0,Lx))
-    savefig("V_slice_x-$i.pdf")
-    plot(x, ϕ[:,ny], xlabel = L"$x$", ylabel = L"$\phi(x,10,t)$", margin=2mm, legend = false, xlims=(0,Lx))
-    savefig("phi_slice_x-$i.pdf")
-    plot(y, U[nx,:], xlabel = L"$y$", ylabel = L"$u(10,y,t)$", margin=2mm, legend = false, xlims=(0,Ly))
-    savefig("u_slice_y-$i.pdf")
-    plot(y, V[nx,:], xlabel = L"$y$", ylabel = L"$V(10,y,t)$", margin=2mm, legend = false, xlims=(0,Ly))
-    savefig("V_slice_y-$i.pdf")
-    plot(y, ϕ[nx,:], xlabel = L"$y$", ylabel = L"$\phi(10,y,t)$", margin=2mm, legend = false, xlims=(0,Ly))
-    savefig("phi_slice_y-$i.pdf")
-    writedlm("x.csv", x)
-    writedlm("y.csv", y)
-    writedlm("ux-$i.csv", U[:,ny])
-    writedlm("uy-$i.csv", U[nx,:])
-end
-
-function draw_slices(x, y, U, ϕ, i, nx, ny, Lx, Ly)
-    gr(); plot() # Load GR plotting backend and clear previous plots
-    default(titlefont = (18, "Computer Modern"), guidefont = (26, "Computer Modern"), tickfont = (18, "Computer Modern"))
-    plot(x, U[:,ny], xlabel = L"$x$", ylabel = L"$u(x,10,t)$", margin=2mm, legend = false, xlims=(0,Lx))
-    savefig("u_slice_x-$i.pdf")
-    plot(x, ϕ[:,ny], xlabel = L"$x$", ylabel = L"$\phi(x,10,t)$", margin=2mm, legend = false, xlims=(0,Lx))
-    savefig("phi_slice_x-$i.pdf")
-    plot(y, U[nx,:], xlabel = L"$y$", ylabel = L"$u(10,y,t)$", margin=2mm, legend = false, xlims=(0,Ly))
-    savefig("u_slice_y-$i.pdf")
-    plot(y, ϕ[nx,:], xlabel = L"$y$", ylabel = L"$\phi(10,y,t)$", margin=2mm, legend = false, xlims=(0,Ly))
-    savefig("phi_slice_y-$i.pdf")
+    default(titlefont = (18, "Computer Modern"), guidefont = (18, "Computer Modern"), tickfont = (18, "Computer Modern"))
+    U = readdlm("U-0.csv")
+    plot(x, U[:,ny], xlabel = L"$x$", ylabel = L"$u(x,10,t)$", linecolor = :black, grid = false, margin=3mm, legend = false, xlims=(0,Lx), ylims=(0,1))
+    for i in plot_times[2:2:end]
+        u = readdlm("ux-$i.csv")
+        plot!(x, u, linecolor = :black, linestyle = :dash)
+    end
+    # plot!([7.595174442,7.595174442], [0,1], linecolor = :red)
+    # plot!([12.404825558,12.404825558], [0,1], linecolor = :red)
+    savefig("u_slice_x.pdf")
+    plot(y, U[nx,:], xlabel = L"$y$", ylabel = L"$u(10,y,t)$", linecolor = :black, grid = false, margin=3mm, legend = false, xlims=(0,Ly), ylims=(0,1))
+    for i in plot_times[2:2:end]
+        u = readdlm("uy-$i.csv")
+        plot!(y, u, linecolor = :black, linestyle = :dash)
+    end
+    # plot!([7.595174442,7.595174442], [0,1], linecolor = :red)
+    # plot!([12.404825558,12.404825558], [0,1], linecolor = :red)
+    savefig("u_slice_y.pdf")
 end
 
 # Control function for plotting
@@ -72,13 +60,12 @@ function draw()
     U = readdlm("U-0.csv")
     ϕ = readdlm("Phi-0.csv")
     draw_heat(x, y, U, ϕ, 0, Lx, Ly)
-    draw_slices(x, y, U, ϕ, 0, nx, ny, Lx, Ly)
+    draw_slices(x, y, nx, ny, Lx, Ly, plot_times)
     for i in plot_times
         U = readdlm("U-$i.csv")
         V = readdlm("V-$i.csv")
         ϕ = readdlm("Phi-$i.csv")
         draw_heat(x, y, U, V, ϕ, i, Lx, Ly)
-        draw_slices(x, y, U, V, ϕ, i, nx, ny, Lx, Ly)
     end
 end
 
